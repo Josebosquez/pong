bad354de23d4b0c75f01651d702cb99084415fa7
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-16 19:41:10 -0600

    its something

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const PADDLE_WIDTH = 20;
 // Size of the ball (in px)
 const BALL_SIZE = 20;
 
+
 // Get the computer paddle element
 const computerPaddle = document.querySelector('.computer-paddle');
 
@@ -16,9 +17,15 @@ const computerPaddle = document.querySelector('.computer-paddle');
 let computerPaddleYPosition = 0;
 let computerPaddleYVelocity = 1;
 
+// Ball position and velocity
+const moveBall = document.querySelector('.ball');
+moveBall.style.borderRadius = "50%"
+let ballHorizontalMov = .5;
+let ballVerticalMov = .5;
+let ballVelocity = 1;
+
 // Update the pong world
 function update() {
-
     // Update the computer paddle's position
     computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
 
@@ -27,6 +34,34 @@ function update() {
 
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
+//------------------------------------------------------------------------------------------------------------------
+//                        ball  
+    ballPosition = ballVerticalMov + ballVelocity
+    
+    ballHorizontalMov++;
+    ballVerticalMov++;
+
+    if (ballHorizontalMov >= GAME_AREA_WIDTH){
+        ballHorizontalMov = ballVelocity * -1
+    }
+    if (ballVerticalMov >= GAME_AREA_HEIGHT){
+        ballVerticalMov -= 25
+    }
+    if (ballHorizontalMov <= GAME_AREA_WIDTH){
+        ballHorizontalMov += 1
+    }
+    if (ballVerticalMov <= GAME_AREA_HEIGHT){
+        ballVerticalMov += 1
+    }
+
+    ballPosition = ballPosition % (GAME_AREA_HEIGHT - GAME_AREA_WIDTH);
+    
+    //this is the ball movement
+    ballHorizontalMov = ballHorizontalMov + ballVelocity;
+    ballVerticalMov = ballVerticalMov + ballVelocity;
+    
+    moveBall.style.top = `${ballVerticalMov}px`;
+    moveBall.style.left = `${ballHorizontalMov}px`;
 }
 
 // Call the update() function everytime the browser is ready to re-render
